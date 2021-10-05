3249885ce6f83cd72acd9d35619ff4c3e9cca1f6
Valeria <50666836+valeriaGratt@users.noreply.github.com>
2021-10-05 14:30:36 +0300

    Add files via upload

---
+++ lesson2.2_file_input.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import os
+import time
+try:
+    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/file_input.html"
+    browser.get(link)
+   
+    input1 = browser.find_element_by_name("firstname")
+    input1.send_keys('My')
+    input2 = browser.find_element_by_name("lastname")
+    input2.send_keys('My')
+    input3 = browser.find_element_by_name("email")
+    input3.send_keys('My')
+    current_dir = os.path.abspath(os.path.dirname(file))
+    file_path = os.path.join(current_dir, 'Lol.txt')            
+    file_btn = browser.find_element_by_id("file")
+    file_btn.send_keys(file_path)
+    
+    button2 = browser.find_element_by_class_name("btn")
+    button2.click()
+    assert True
+
+finally:
+    time.sleep(10)
+    browser.quit()
